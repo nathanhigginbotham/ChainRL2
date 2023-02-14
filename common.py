774d29e3e445922b3387a7c8a3da35d3f13f47d6
trahxam <ucapmh0@ucl.ac.u>
2023-02-14 11:16:24 +0000

    cleaning up a bit

--- common.py
+++ common.py
@@ -11,4 +11,10 @@ def make_env(env_name, env_seed=None):
     if env_name == 'NetworkManagement-v1-100':
         env = or_gym.make('NetworkManagement-v1', env_seed=env_seed, num_periods=100)
 
+    if env_name == 'NetworkManagement-v1-100-alt':
+        env = or_gym.make('NetworkManagement-v1', env_seed=env_seed, num_periods=100)
+
     return env
+
+
+make_env('NetworkManagement-v1-100-alt')
