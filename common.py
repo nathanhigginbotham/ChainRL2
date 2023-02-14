e4d453b1a6bf15ab3600e674715e34ba25a062c2
trahxam <ucapmh0@ucl.ac.u>
2023-02-14 10:37:34 +0000

    added evaluation script and run visualisation

---
+++ common.py
@@ -0,0 +1,14 @@
+import or_gym
+
+
+def make_env(env_name, env_seed=None):
+    if env_name == 'InvManagement-v1':
+        env = or_gym.make('InvManagement-v1', env_seed=env_seed)
+
+    if env_name == 'NetworkManagement-v1':
+        env = or_gym.make('NetworkManagement-v1', env_seed=env_seed)
+
+    if env_name == 'NetworkManagement-v1-100':
+        env = or_gym.make('NetworkManagement-v1', env_seed=env_seed, num_periods=100)
+
+    return env
